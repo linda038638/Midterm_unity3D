5c01e47bd8fedefefa17b6ffb1ecd810f65df6e1
Snow.fishb <72215312+linda038638@users.noreply.github.com>
2022-11-05 17:53:50 +0800

    Add menu

--- First3D_BulletGame/Assets/Script/CameraConttroller.cs
+++ First3D_BulletGame/Assets/Script/CameraConttroller.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+/// <summary>
+/// 
+/// 官方文件 https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+/// </summary>
+
 
 namespace Misun
 {
 
     public class CameraConttroller : MonoBehaviour
     {
-        [SerializeField]
-        private Transform player;
+        [SerializeField] 
+        private Transform player;//玩家位置資訊
         private float mouseX, mouseY; //X方向是左右移動，Y方向是上下移動
         [SerializeField, Header("滑鼠靈敏度")]
-        private float controlSpeed;
+        private float RLcontrolSpeed;
+        [SerializeField]
+        private float UDcontrolSpeed;
         private float LimitRotation;
 
         private void Update()
@@ -20,11 +27,16 @@ namespace Misun
 
         private void CameraControl()
         {
-            mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * controlSpeed;
-            mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * controlSpeed;
+            
+            mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * RLcontrolSpeed;
+            mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * UDcontrolSpeed;
+           
+
             LimitRotation -= mouseY;
-            LimitRotation = Mathf.Clamp(LimitRotation, -25f, 25f);
+            LimitRotation = Mathf.Clamp(LimitRotation, -90f, 90f);
+           
             player.Rotate(Vector3.up * mouseX);
+            
             transform.localRotation = Quaternion.Euler(LimitRotation, 0, 0);
         }
         
